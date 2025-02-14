e65c37740d6d8ed4a44de12925a7f1940e6d250a
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-02-14 18:12:36 +0000

    Use env var for proxy address

--- test/debug.spec.js
+++ test/debug.spec.js
@@ -4,7 +4,7 @@ import { setGlobalDispatcher, ProxyAgent } from "undici";
 describe('Debug', () => {
   it('should validate assumptions', async () => {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-    const dispatcher = new ProxyAgent({uri: "http://localhost:3128" });
+    const dispatcher = new ProxyAgent({uri: process.env.HTTP_PROXY });
     setGlobalDispatcher(dispatcher);
 
     const response = await fetch('https://sareportingpoc.blob.core.windows.net/wcag/wave.min.js')
