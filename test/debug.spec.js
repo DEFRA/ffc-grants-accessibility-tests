bbe4a4005fede763af258c040478965afa598d76
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-02-14 18:08:07 +0000

    Validate assumptions

--- test/debug.spec.js
+++ test/debug.spec.js
@@ -1,7 +1,12 @@
 import { browser, expect } from '@wdio/globals'
+import { setGlobalDispatcher, ProxyAgent } from "undici";
 
 describe('Debug', () => {
   it('should validate assumptions', async () => {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    const dispatcher = new ProxyAgent({uri: "http://localhost:3128" });
+    setGlobalDispatcher(dispatcher);
+
     const response = await fetch('https://sareportingpoc.blob.core.windows.net/wcag/wave.min.js')
     expect(response.status).toEqual(200)
   });
